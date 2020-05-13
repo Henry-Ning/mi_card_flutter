dc6e326c92a0e773b199f560537b4b27481c2375
Henry-Ning <henryhuning@gmail.com>
2020-05-13 05:02:29 -0700

    501AM 5-13-2020 - 49. Adding Material Icons with the Icon Widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,31 +12,76 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(children: <Widget>[
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('images/download.png'),
-            ),
-            Text(
-              'Henry Ning',
-              style: TextStyle(
-                fontFamily: 'Pacifico',
-                fontSize: 30,
-                color: Colors.white,
-                fontWeight: FontWeight.bold
-              )
-            ),
-            Text(
-              'Flutter Developer',
-              style: TextStyle(
-                fontFamily: 'SourceSansPro',
-                color: Colors.teal[100],
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.5,
-                fontSize: 25
-              ),
-            ),
-          ]),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/download.png'),
+                ),
+                Text('Henry Ning',
+                    style: TextStyle(
+                        fontFamily: 'Pacifico',
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold)),
+                Text(
+                  'Flutter Developer',
+                  style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      color: Colors.teal[100],
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.5,
+                      fontSize: 25
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: double.infinity,
+                ),
+                Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      Text(
+                        '  +1-702-290-7298',
+                        style: TextStyle(
+                            fontSize: 20
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      Text(
+                        '  henryhuning@gmail.com',
+                        style: TextStyle(
+                            fontSize: 20
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+          ),
         ),
       ),
     );
