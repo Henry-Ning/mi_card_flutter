cf3b2d0b85372a527c2f71bce1fc142766504188
Henry-Ning <henryhuning@gmail.com>
2020-05-13 06:05:20 -0700

    605AM 5-13-2020 - 50. Flutter Card & ListTile Widgets 13min

--- lib/main.dart
+++ lib/main.dart
@@ -13,74 +13,67 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage('images/download.png'),
-                ),
-                Text('Henry Ning',
-                    style: TextStyle(
-                        fontFamily: 'Pacifico',
-                        fontSize: 30,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold)),
-                Text(
-                  'Flutter Developer',
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 80,
+                backgroundImage: AssetImage('images/download.png'),
+              ),
+              Text('Henry Ning',
                   style: TextStyle(
-                      fontFamily: 'SourceSansPro',
-                      color: Colors.teal[100],
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 2.5,
-                      fontSize: 25
-                  ),
+                      fontFamily: 'Pacifico',
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold)),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.teal[100],
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5,
+                    fontSize: 25),
+              ),
+              SizedBox(
+                height: 30,
+                width: 200,
+                child: Divider(
+                  color: Colors.teal.shade200,
+                  thickness: 2,
                 ),
-                SizedBox(
-                  height: 20,
-                  width: double.infinity,
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '  +1-702-290-7298',
+                      style: TextStyle(fontSize: 20),
+                    ),
                 ),
-                Container(
+              ),
+              Card(
                   color: Colors.white,
-                  padding: EdgeInsets.all(10),
                   margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                      ),
-                      Text(
-                        '  +1-702-290-7298',
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                        'henryhuning@gmail.com',
                         style: TextStyle(
-                            fontSize: 20
+                            fontSize: 20,
                         ),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
-                Container(
-                  color: Colors.white,
-                  padding: EdgeInsets.all(10),
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(
-                        Icons.email,
-                        color: Colors.teal,
-                      ),
-                      Text(
-                        '  henryhuning@gmail.com',
-                        style: TextStyle(
-                            fontSize: 20
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+              ),
+            ],
           ),
         ),
       ),
